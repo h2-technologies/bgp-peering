da31b0be1d6e7cac05fe83534b0a0737a0293475
ahadley1124 <admin@austinh.dev>
2025-12-28 16:47:04 -0800

    Redo Website

--- src/main.rs
+++ src/main.rs
@@ -23,13 +23,10 @@ enum Route {
 fn switch(routes: Route) -> Html {
     match routes {
         Route::Home => html! { <site::home::Home /> },
-        Route::About => html! { <site::about::About /> },
-        Route::Contact => html! { <site::contact::Contact /> },
-        Route::Pricing => html! { <site::pricing::Pricing /> },
-        Route::Policy => html! {
-            <iframe src="/Amateur_Virtual_Internet_Exchange_Policy_V1.0.pdf" width="100%" height="800px" title="Exchange Policy PDF" />
-        },
-        Route::Peers => html! { <site::peers::Peers /> },
+        Route::PeeringSessions => html! { <site::peering_sessions::PeeringSessions /> },
+        Route::PeeringSetup => html! { <site::peering_setup::PeeringSetup /> },
+        Route::PNI => html! { <site::pni::PNI /> },
+        Route::AllPeeringLocations => html! { <site::all_peering_locations::AllPeeringLocations /> },
         Route::NotFound => html! { <h1>{ "404" }</h1> },
     }
 }
